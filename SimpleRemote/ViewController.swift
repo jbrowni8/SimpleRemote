315704225b78d3957c815194affc388f8907aaa2
James Browning <jbxmv@mail.missouri.edu>
2017-02-01 13:33:15 -0500

    Working on VC 1 Feb 2017, 1333

--- SimpleRemote/ViewController.swift
+++ SimpleRemote/ViewController.swift
@@ -13,14 +13,50 @@ class ViewController: UIViewController {
     @IBOutlet weak var power: UILabel!
     @IBOutlet weak var vol: UILabel!
     @IBOutlet weak var chan: UILabel!
+    var digits : String = ""
+    var counter = 0
     
     @IBAction func powerOn(_ sender: UISwitch) {
         power.text = (sender.isOn ? "On" : "Off")
+        if power.text=="On" {
+            chan.text = "\(000)"
+        } else {
+            vol.text = "\(0)"
+            chan.text = "Off"
+        }
     }
     
     @IBAction func deltaVol(_ sender: UISlider) {
         if power.text=="On" {
             vol.text = "\(Int(sender.value))"
+        }
+    }
+    
+    @IBAction func chanChange(_ sender: UIButton) {
+        if power.text=="On"{
+            if let text = sender.titleLabel?.text {
+                if let currentChan = Int(chan.text!) {
+                    if text == "Ch+" && currentChan < 999{
+                        chan.text = "\(currentChan+1)"
+                    } else if text == "Ch-" && currentChan > 0 {
+                        chan.text = "\(currentChan-1)"
+                    }
+                }
+            }
+        }
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if power.text == "On"{
+            if let digit = sender.titleLabel?.text {
+                counter += 1
+                digits.append(digit)
+            }
+            if counter == 3 {
+                chan.text = "\(digits)"
+                digits = ""
+                counter = 0
+            }
         }
     }
     
